52e2d7da32d8b79615ddd2317bfb5ae17bbbf700
RoMeRO93 <122970841+RoMeRO93@users.noreply.github.com>
2023-05-19 13:57:39 +0300

    Add project files.

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Introduceti primul numar a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Introduceti al doilea numar b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("a={0}, b={1}", a, b);
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.Write("Dupa interschimbare a={0}, b={1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
